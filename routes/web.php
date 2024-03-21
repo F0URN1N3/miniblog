146e06bb7cb4598304ccc8255e985f6a414fcab9
F0URN1N3 <twf0urn1n3@gmail.com>
2024-03-21 09:58:03 +0000

    build Controller, Model, route & Vite

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserAuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//首頁
+// Route::get('/', 'HomeController@indexPage');
+Route::get('/', [HomeController::class, 'indexPage']);
 
-Route::get('/', function () {
-    return view('welcome');
+//使用者
+// Route::get('/user/auth/sign-up', 'UserAuthController@signUpPage');
+// Route::post('/user/auth/sign-up', 'UserAuthController@signUpProcess');
+// Route::get('/user/auth/sign-in', 'UserAuthController@signInPage');
+// Route::post('/user/auth/sign-in', 'UserAuthController@signInProcess');
+// Route::get('/user/auth/sign-out', 'UserAuthController@signOut');
+//  ↓group化寫法
+// Route::group(['prefix' => 'user'], function(){
+//     //使用者驗證
+//     Route::group(['prefix' => 'auth'], function(){
+//         Route::get('/sign-up', 'UserAuthController@signUpPage');
+//         Route::post('/sign-up', 'UserAuthController@signUpProcess');
+//         Route::get('/sign-in', 'UserAuthController@signInPage');
+//         Route::post('/sign-in', 'UserAuthController@signInProcess');
+//         Route::get('/sign-out', 'UserAuthController@signOut');
+//     });
+// });
+
+//新版group化寫法
+Route::controller(UserAuthController::class)->group(function(){
+    Route::prefix('user')->group(function(){
+        Route::prefix('auth')->group(function(){
+            Route::get('sign-up', 'signUpPage');
+            Route::post('sign-up', 'signUpProcess');
+            Route::get('sign-in', 'signInPage');
+            Route::post('sign-in', 'signInProcess');
+            Route::get('sign-out', 'signOut');
+        });
+    });
 });
