0d6381f3795c5d56373f9d677bfb351b52d4a95e
root <twf0urn1n3@gmail.com>
2024-03-24 07:39:47 +0000

    sign-up validation working 1

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ShareData;
+use Illuminate\Support\Facades\Validator;
 
 class UserAuthController extends Controller
 {
@@ -15,6 +16,47 @@ class UserAuthController extends Controller
             'name'=> $name,
         ];
         return view('user.sign-up', $binding);
+    }
+
+    public function signUpProcess(){
+        //接收輸入資料
+        $input = request()-> all();
+
+        //驗證規則
+        $rules = [
+            //暱稱
+            'name' => [
+                'required',
+                'max:50',
+            ],
+            //帳號(E-mail)
+            'account' => [
+                'required',
+                'max:50',
+                'email',
+            ],
+            //密碼
+            'password' => [
+                'required',
+                'min:5',
+            ],
+            //密碼驗證
+            'password_confirm' => [
+                'required',
+                'same:password',
+                'min:5'
+            ],
+        ];
+
+        //驗證資料
+        $validator = Validator::make($input, $rules);
+
+        //資料錯誤處理
+        if($validator->fails()){
+            return redirect('user/auth/sign-up')-> withErrors($validator);
+        }
+
+        exit;
     }
 
 }
--- resources/views/user/sign-up.blade.php
+++ resources/views/user/sign-up.blade.php
@@ -4,15 +4,28 @@
 
 @section('content')
 <form id="form1" method="POST" action="">
+{{ csrf_field() }}
 <div class="login_form">
     <div class="login_title">註冊</div>
-    <div class="login_label">帳號</div>
+    <div class="login_label">暱稱</div>
+    <div class="login_textbox">
+        <input name="name" class="form_textbox" type="text" placeholder="請輸入暱稱"/>
+    </div>
+    <div class="login_label">帳號(必須為E-mail)</div>
     <div class="login_textbox">
         <input name="account" class="form_textbox" type="text" placeholder="請輸入帳號"/>
     </div>
     <div class="login_label">密碼</div>
     <div class="login_textbox">
         <input name="password" class="form_textbox" type="password" placeholder="請輸入密碼"/>
+    </div>
+    <div class="login_label">密碼確認</div>
+    <div class="login_textbox">
+        <input name="password_confirm" class="form_textbox" type="password" placeholder="請確認密碼"/>
+    </div>
+    <div class="login_error">
+        {{-- 錯誤訊息顯示處 --}}
+        @include('layout.ValidatorError')
     </div>
     <div class="btn_group">
         <button type="submit" class="btn btn-primary btn_login">註冊</button>
