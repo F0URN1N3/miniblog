c87336afa481c6d03908e0360700bfe1de3e4ba6
F0URN1N3 <twF0URN1N3@gmail.com>
2026-01-04 15:55:38 +0800

    Fix: Force HTTPS for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // 檢查是否在正式環境 (Render 上通常會設為 production)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https'); // 這樣就不會有紅線了
+        }
         //避免Paginator出現不合比例的換頁符號
         Paginator::useBootstrap();
     }
